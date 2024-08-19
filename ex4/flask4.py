a94fefdc18bc1d0b2ea1f7683a2eb216c1ace065
user <lynnyk.first@gmail.com>
2024-08-19 11:09:29 +0300

    exers4

--- ex4/flask4.py
+++ ex4/flask4.py
@@ -1,17 +1,20 @@
 from flask import Flask, jsonify
 from webargs.flaskparser import use_kwargs
-from webargs import fields
+from webargs import fields, validate
 import sqlite3
 
 app = Flask(__name__)
 
+
 def connector():
     return sqlite3.connect('Chinook.sqlite')
+
 
 def result_and_close(cursor, conn):
     results = cursor.fetchall()
     conn.close()
     return results
+
 
 @app.route("/order_price")
 def order_price():
@@ -31,6 +34,7 @@ def order_price():
     data = [{"BillingCountry": row[0], "TotalPrice": row[1]} for row in results]
 
     return jsonify(data)
+
 
 @app.route("/order_price_by_country")
 @use_kwargs(
@@ -61,10 +65,11 @@ def order_price_by_country(country):
 
     return jsonify(data)
 
+
 @app.route("/get_all_info_about_track")
 @use_kwargs(
     {
-        "track_id": fields.Int(validate=fields.Range(min=1, max=500), missing=None)
+        "track_id": fields.Int(validate=validate.Range(min=1, max=500), missing=None)
     },
     location="query"
 )
@@ -92,7 +97,8 @@ def get_all_info_about_track(track_id):
         cursor.execute(query, (track_id,))
         results = cursor.fetchall()
         conn.close()
-        keys = ['TrackName', 'AlbumTitle', 'MediaTypeName', 'GenreName', 'Composer', 'Milliseconds', 'Bytes', 'UnitPrice']
+        keys = ['TrackName', 'AlbumTitle', 'MediaTypeName', 'GenreName', 'Composer', 'Milliseconds', 'Bytes',
+                'UnitPrice']
         data = [dict(zip(keys, entry)) for entry in results]
         return jsonify(data)
 
@@ -103,6 +109,7 @@ def get_all_info_about_track(track_id):
     total_hours = total_time / (1000 * 60 * 60)
     conn.close()
     return jsonify({"TotalHours": total_hours})
+
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
